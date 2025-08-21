3ad0462edd4b72aaa9efa5b498d31267b1c7d752
Drunk Fly Team <drunkflyteam@gmail.com>
2025-08-21 11:59:13 +0300

    Use pour_wrapper.

--- _open_msvc.cmd
+++ _open_msvc.cmd
@@ -8,14 +8,14 @@ if not exist build mkdir build
 if not exist build\msvc mkdir build\msvc
 cd build\msvc || exit /B 1
 
-call "%~dp0tools\cmake-3.31.4.cmd" ^
-    -A Win32 ^
-    "%~dp0" ^
-    || exit /B 1
+call "%~dp0tools\pour_wrapper_windows.exe" ^
+    --run cmake-3.31.4 ^
+        -A Win32 ^
+        "%~dp0" ^
+        || exit /B 1
 
 cd "%~dp0" || exit /B 1
 
 :skip_cmake
 
 start build\msvc\flinux.sln
-echo Done
