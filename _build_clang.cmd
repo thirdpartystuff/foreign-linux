680ad577f7210e70161bb9a70bbda38680e76205
Drunk Fly Team <drunkflyteam@gmail.com>
2025-08-25 00:11:02 +0300

    Use Ninja as build tool.

--- _build_clang.cmd
+++ _build_clang.cmd
@@ -1,13 +1,14 @@
 @"%~dp0tools\pour_wrapper_windows.exe" --script "%0" && exit /B 0 || exit /B 1
 
 pour.require("clang-4.0.0")
+pour.require("ninja")
 
 pour.chdir(SCRIPT_DIR..'/build/clang')
 
 if not pour.file_exists('flinux.exe') then
 
     pour.run('cmake-3.31.4',
-            '-G', 'MinGW Makefiles',
+            '-G', 'Ninja',
             '-DCMAKE_BUILD_TYPE=Release',
             '-DCMAKE_SYSTEM_NAME=Windows-GNU',
             '-DCMAKE_C_COMPILER=clang',
