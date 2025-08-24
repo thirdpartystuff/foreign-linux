016682cac9a9b562adf2b06b37d0dfbc8a8e833b
Drunk Fly Team <drunkflyteam@gmail.com>
2025-08-24 19:35:41 +0300

    Fixes for clang compilation.

--- _build_clang.cmd
+++ _build_clang.cmd
@@ -1,6 +1,6 @@
 @echo off
 setlocal
-set PATH=C:\LLVM-4.0.0\bin;C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Bin;%PATH%
+set PATH=D:\Work\clang-4.0.0\bin;%PATH%
 cd "%~dp0" || exit /B 1
 
 if not exist build mkdir build
@@ -12,13 +12,13 @@ if exist flinux.sln goto skip_cmake
 call "%~dp0tools\pour_wrapper_windows.exe" ^
     --run cmake-3.31.4 ^
         -G "MinGW Makefiles" ^
-        -DCMAKE_SYSTEM_NAME=MinGW ^
         -DCMAKE_BUILD_TYPE=Release ^
+        -DCMAKE_SYSTEM_NAME=Windows-GNU ^
         -DCMAKE_C_COMPILER=clang ^
         -DCMAKE_C_COMPILER_WORKS=TRUE ^
         -DCMAKE_C_STANDARD_LIBRARIES="" ^
         -DCMAKE_ASM_MASM_COMPILER="%~dp0tools\uasm32.exe" ^
-        "%~dp0" ^
+        "%~dp0." ^
         || exit /B 1
 
 :skip_cmake
--- src/dbt/x86_inst.h
+++ src/dbt/x86_inst.h
@@ -17,6 +17,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <stddef.h>
 #include <stdbool.h>
 #include <stdint.h>
 
